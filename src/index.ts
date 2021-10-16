5071fdea07b021ffaf169b93ef63d3fb3f927552
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-10-16 18:53:17 +0530

    converted to es6

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 import { stringify } from 'query-string';
 import {
   Identifier,
-  Pagination,
-  Sort,
-  Filter,
+  
   fetchUtils,
   DataProvider,
-  GetListParams,
   GetListResult,
   GetManyReferenceParams,
   GetManyReferenceResult,
+  CreateParams,
+  CreateResult,
+  DeleteManyParams,
+  DeleteManyResult,
+  DeleteParams,
+  DeleteResult,
+  GetManyParams,
+  GetManyResult,
+  GetOneParams,
+  GetOneResult,
+  UpdateManyParams,
+  UpdateManyResult,
+  UpdateParams,
+  UpdateResult,
+  FilterPayload,
+  SortPayload,
+  PaginationPayload,
+  GetListParams,
+  Record,
 } from 'ra-core';
 
 export {
@@ -17,7 +33,8 @@ export {
   fetchJsonWithAuthToken,
 } from './tokenAuthProvider';
 
-const getPaginationQuery = (pagination: Pagination) => {
+const getPaginationQuery = (pagination: PaginationPayload) => {
+  if(pagination.page === 0) return {}
   return {
     page: pagination.page,
     page_size: pagination.perPage,
@@ -25,7 +42,7 @@ const getPaginationQuery = (pagination: Pagination) => {
   };
 };
 
-const getFilterQuery = (filter: Filter) => {
+const getFilterQuery = (filter: FilterPayload) => {
   const { q: search, ...otherSearchParams } = filter;
   return {
     ...otherSearchParams,
@@ -33,35 +50,224 @@ const getFilterQuery = (filter: Filter) => {
   };
 };
 
-export const getOrderingQuery = (sort: Sort) => {
+export const getOrderingQuery = (sort: SortPayload) => {
   const { field, order } = sort;
   return {
     ordering: `${order === 'ASC' ? '' : '-'}${field}`,
   };
 };
 
-export interface CustomDataProvider extends DataProvider {
-  getList: (
+
+
+
+/**
+ *
+ *
+ * @export
+ * @class RoundwareDataProvider
+ * @implements {DataProvider}
+ */
+export class RoundwareDataProvider implements DataProvider {
+  apiUrl: string;
+  httpClient: Function;
+  paginateAllByDefault: boolean;
+  constructor(
+    apiUrl: string,
+    httpClient: Function = fetchUtils.fetchJson,
+    paginateAllByDefault = false
+  ) {
+    this.apiUrl = apiUrl;
+    this.httpClient = httpClient;
+    this.paginateAllByDefault = paginateAllByDefault;
+  }
+
+
+
+  /**
+   *
+   */
+  async getList<RecordType extends Record = Record>(
     resource: string,
     params: GetListParams,
-    paginate?: boolean
-  ) => Promise<GetListResult>;
-  getManyReference: (
+    paginate: boolean = [`assets`].includes(resource) ||
+      this.paginateAllByDefault
+  ): Promise<GetListResult<RecordType>> {
+    let query = {
+      ...getFilterQuery(params.filter),
+      ...getOrderingQuery(params.sort),
+      ...(paginate && getPaginationQuery(params.pagination)),
+      ...(this.isEligibleForCaching(params.filter) && this.getCachingQuery(resource)),
+    };
+
+    const url = `${this.apiUrl}/${resource}/?${stringify(query)}`;
+
+    
+
+    let { json } = await this.httpClient(url);
+    
+    json = this.normalizeApiResponse(json)
+    
+
+    this.isEligibleForCaching(params.filter) && (json = this.mergeWithCachedData<RecordType>(resource, json));
+
+    return {
+      data: json,
+      total: json.length,
+    };
+  }
+
+  normalizeApiResponse(data: any[] | { results: any[] }) {
+    return Array.isArray(data) ? data : data.results
+  }
+
+  // if there is already a time filter then it should not be cached
+  isEligibleForCaching(filter: FilterPayload) {
+    return false // temporarily disabling caching
+    let eligible = true;
+    [`created__gte`, `created__lte`, `start_time__gte`, `start_time__lte`].forEach((k) => {
+      if (k in filter) eligible = false;
+    })
+    return eligible
+  }
+
+  
+  cachedResources = new Map<string, { lastFetched: Date, data: any[] }>();
+  getCachingQuery(resource: string) {
+    const cachedResource = this.cachedResources.get(resource);
+    if (!cachedResource) return {};
+    let query = {};
+
+    switch (resource) {
+      case `assets`:
+        query = {
+          created__gte: cachedResource.lastFetched.toISOString()
+        }
+        break;
+    
+      case `listenevents`:
+        query = {
+          start_time__gte: cachedResource.lastFetched.toISOString()
+        }
+        break;
+      default:
+        break;
+    }
+    return query;
+  };
+
+  mergeWithCachedData<RecordType extends Record>(resource: string, data: any[]): GetListResult<RecordType>["data"] {
+
+    if (![`assets`, `listenevents`].includes(resource)) return data;
+    const cachedResource = this.cachedResources.get(resource);
+
+    if (!cachedResource) {
+      this.cachedResources.set(resource, {
+        lastFetched: new Date(),
+        data,
+      })
+      return data;
+    }
+    const newData = cachedResource.data.concat(data);
+    console.info(`used cached data for ${resource}`);
+    this.cachedResources.set(resource, {
+      lastFetched: new Date(),
+      data: newData
+    })
+    return newData;
+  }
+
+
+  async getOne<RecordType extends Record>(
+    resource: string,
+    { id, ...query }: GetOneParams
+  ): Promise<GetOneResult<RecordType>> {
+    const data = await this.getOneJson(resource, id, query);
+    return {
+      data,
+    };
+  }
+  async getMany<RecordType extends Record>(
+    resource: string,
+    params: GetManyParams
+  ): Promise<GetManyResult<RecordType>> {
+    return Promise.all(
+      params.ids.map(id => this.getOneJson(resource, id))
+    ).then(data => ({ data }));
+  }
+  async getManyReference<RecordType extends Record>(
     resource: string,
     params: GetManyReferenceParams,
-    paginate?: boolean
-  ) => Promise<GetManyReferenceResult>;
-}
+    paginate: boolean = false
+  ): Promise<GetManyReferenceResult<RecordType>> {
+    let query = {
+      ...getFilterQuery(params.filter),
+      ...(paginate && getPaginationQuery(params.pagination)),
+      ...getOrderingQuery(params.sort),
+      [params.target]: params.id,
+    };
 
-export default (
-  apiUrl: string,
-  httpClient: Function = fetchUtils.fetchJson,
-  paginateAllByDefault: boolean = false
-): CustomDataProvider => {
-  const getOneJson = async (
+    const url = `${this.apiUrl}/${resource}/?${stringify(query)}`;
+
+    const { json } = await this.httpClient(url);
+    return {
+      data: paginate ? json.results : json,
+      total: paginate ? json.count : json?.length,
+    };
+  }
+  async update<RecordType extends Record>(resource: string, params: UpdateParams): Promise<UpdateResult<RecordType>> {
+    const { json } = await this.httpClient(
+      `${this.apiUrl}/${resource}/${params.id}/`,
+      {
+        method: 'PATCH',
+        body: JSON.stringify(params.data),
+      }
+    );
+    return { data: json };
+  }
+  async updateMany(
+    resource: string,
+    params: UpdateManyParams
+  ): Promise<UpdateManyResult> {
+    return Promise.all(
+      params.ids.map(id =>
+        this.httpClient(`${this.apiUrl}/${resource}/${id}/`, {
+          method: 'PATCH',
+          body: JSON.stringify(params.data),
+        })
+      )
+    ).then(responses => ({ data: responses.map(({ json }) => json.id) }));
+  }
+  async create<RecordType extends Record>(resource: string, params: CreateParams): Promise<CreateResult<RecordType>> {
+    const { json } = await this.httpClient(`${this.apiUrl}/${resource}/`, {
+      method: 'POST',
+      body: JSON.stringify(params.data),
+    });
+    return {
+      data: { ...json },
+    };
+  }
+  async delete<RecordType extends Record>(resource: string, params: DeleteParams): Promise<DeleteResult<RecordType>> {
+    return this.httpClient(`${this.apiUrl}/${resource}/${params.id}/`, {
+      method: 'DELETE',
+    }).then(() => ({ data: params.previousData }));
+  }
+  async deleteMany(
+    resource: string,
+    params: DeleteManyParams
+  ): Promise<DeleteManyResult> {
+    return Promise.all(
+      params.ids.map(id =>
+        this.httpClient(`${this.apiUrl}/${resource}/${id}/`, {
+          method: 'DELETE',
+        })
+      )
+    ).then(responses => ({ data: responses.map(({ json }) => json.id) }));
+  }
+
+  getOneJson = async (
     resource: string,
     id: Identifier,
-    filterQuery: Filter = {}
+    filterQuery: FilterPayload = {}
   ) => {
     // resources which require session_id
     if ([`projects`].includes(resource))
@@ -69,127 +275,13 @@ export default (
         ...filterQuery,
         session_id: 1,
       };
-    
-    let results = await httpClient(
-      `${apiUrl}/${resource}/${id}/?${stringify(getFilterQuery(filterQuery))}`
-    ).then((response: Response) => response.json);
 
-    
+    let results = await this.httpClient(
+      `${this.apiUrl}/${resource}/${id}/?${stringify(
+        getFilterQuery(filterQuery)
+      )}`
+    ).then((response: Response) => response.json);
 
     return results;
   };
-
-  return {
-    getList: async (
-      resource,
-      params,
-      paginate: boolean = ([`assets`].includes(resource)) || paginateAllByDefault
-    ) => {
-      
-      let query = {
-        ...getFilterQuery(params.filter),
-        ...getOrderingQuery(params.sort),
-        ...(paginate && getPaginationQuery(params.pagination)),
-      };
-
-      const url = `${apiUrl}/${resource}/?${stringify(query)}`;
-
-      let { json } = await httpClient(url);
-
-     
-      if ([`listenevents`].includes(resource) && "project_id" in query) {
-        const sessionsQuery = {
-          ...getFilterQuery(params.filter)
-        }
-        let { json: assets } = await httpClient(`${apiUrl}/assets/?${stringify(sessionsQuery)}`);
-        let filteredArray: unknown[] = Array.isArray(json) ? json : json.results;
-        // @ts-ignore
-        filteredArray = filteredArray.filter(r => assets.some(s => s?.id === r?.asset_id));
-        if (Array.isArray(json)) json = filteredArray
-        else json.results = filteredArray
-        console.log(`${resource} Filtered by asset_id belonging to that project, because server doesn't filter by project_id`)
-      }
-
-      return {
-        data: paginate ? json.results : json,
-        total: paginate ? json.count : json.length,
-      };
-    },
-
-    getOne: async (resource, { id, ...query }) => {
-      const data = await getOneJson(resource, id, query);
-      return {
-        data,
-      };
-    },
-
-    getMany: (resource, params) => {
-      return Promise.all(
-        params.ids.map(id => getOneJson(resource, id))
-      ).then(data => ({ data }));
-    },
-
-    getManyReference: async (
-      resource,
-      params,
-      paginate: boolean = paginateAllByDefault
-    ) => {
-      let query = {
-        ...getFilterQuery(params.filter),
-        ...(paginate && getPaginationQuery(params.pagination)),
-        ...getOrderingQuery(params.sort),
-        [params.target]: params.id,
-      };
-
-      const url = `${apiUrl}/${resource}/?${stringify(query)}`;
-
-      const { json } = await httpClient(url);
-      return {
-        data: paginate ? json.results : json,
-        total: paginate ? json.count : json?.length,
-      };
-    },
-
-    update: async (resource, params) => {
-      const { json } = await httpClient(`${apiUrl}/${resource}/${params.id}/`, {
-        method: 'PATCH',
-        body: JSON.stringify(params.data),
-      });
-      return { data: json };
-    },
-
-    updateMany: (resource, params) =>
-      Promise.all(
-        params.ids.map(id =>
-          httpClient(`${apiUrl}/${resource}/${id}/`, {
-            method: 'PATCH',
-            body: JSON.stringify(params.data),
-          })
-        )
-      ).then(responses => ({ data: responses.map(({ json }) => json.id) })),
-
-    create: async (resource, params) => {
-      const { json } = await httpClient(`${apiUrl}/${resource}/`, {
-        method: 'POST',
-        body: JSON.stringify(params.data),
-      });
-      return {
-        data: { ...json },
-      };
-    },
-
-    delete: (resource, params) =>
-      httpClient(`${apiUrl}/${resource}/${params.id}/`, {
-        method: 'DELETE',
-      }).then(() => ({ data: params.previousData })),
-
-    deleteMany: (resource, params) =>
-      Promise.all(
-        params.ids.map(id =>
-          httpClient(`${apiUrl}/${resource}/${id}/`, {
-            method: 'DELETE',
-          })
-        )
-      ).then(responses => ({ data: responses.map(({ json }) => json.id) })),
-  };
-};
+}
